20c364bd0ec454ff2fa5393ae5eb8d94f930c25c
glaucia86 <glaucia_lemos86@hotmail.com>
2020-03-03 20:41:51 -0300

    Removendo o pacote 'body-parser' e mudanças no arquivo 'app.js'

--- api/src/app.js
+++ api/src/app.js
@@ -6,7 +6,6 @@
  */
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
@@ -18,9 +17,9 @@ const app = express();
 const index = require('./routes/index'); // linha de código temporário
 // const productRoute = require('./routes/product.routes');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
 app.use(index); // linha de código temporário
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get('/api', (req, res) => {
   res.status(200).send({
     success: 'true',
-    message: 'Seja bem-vindo(a) a API Node.js + PostgreSQL',
+    message: 'Seja bem-vindo(a) a API Node.js + PostgreSQL + Azure!',
     version: '1.0.0',
   });
 });
