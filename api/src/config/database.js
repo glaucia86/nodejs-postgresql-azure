34df4bf4c8d815fc5bf11fca728f87d7b8b6f1e2
glaucia86 <glaucia_lemos86@hotmail.com>
2020-03-03 22:07:51 -0300

    Remoção do pacote morgan, inclusão do pacote pg e desnvolvimento do arquivo 'database.js'

---
+++ api/src/config/database.js
@@ -0,0 +1,12 @@
+/**
+ * Arquivo: config/database.js
+ * Descrição: arquivo responsável pelas 'connectionStrings da aplicação: PostgreSQL.
+ * Data: 03/03/2020
+ * Author: Glaucia Lemos
+ */
+
+module.exports = {
+  local: {
+    localUrl: "postgres://glau123@localhost:5432/crud-nodejs-psql"
+  }
+};
---
+++ api/src/controllers/product.controller.js
@@ -0,0 +1,8 @@
+/**
+ * Arquivo: src/controllers/funcionario.controller.js
+ * Descrição: arquivo responsável pelo CRUD da classe 'Product'
+ * Data: 03/03/2020
+ * Author: Glaucia Lemos
+ */
+
+
